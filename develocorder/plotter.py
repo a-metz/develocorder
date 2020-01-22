fabae016975ffabbf48f89bc5f0a69c23d20e344
Alexander Metz <alexander.wt.metz@gmail.com>
2020-01-22 12:13:32 +0100

    add update rate param for plotter

--- develocorder/plotter.py
+++ develocorder/plotter.py
@@ -7,11 +7,12 @@ class Plotter:
     initialized = False
     plots_count = 0
 
-    def __init__(self, xlabel, ylabel, filter_size=None):
+    def __init__(self, xlabel, ylabel, filter_size=None, update_rate=1):
         self.xlabel = xlabel
         self.ylabel = ylabel
         self.filter_size = filter_size
         self.values = []
+        self.update_rate = update_rate
 
         Plotter.plots_count += 1
         self.index = self.plots_count
@@ -28,11 +29,12 @@ class Plotter:
     def __call__(self, value):
         self.values.append(value)
 
-        plt.subplot(self.plots_count, 1, self.index)
-        plt.cla()
-        plt.xlabel(self.xlabel)
-        plt.ylabel(self.ylabel)
-        plt.plot(self.values)
-        if self.filter_size is not None:
-            plt.plot(filter_values(self.values, self.filter_size))
-        plt.pause(0.001)
+        if len(self.values) % self.update_rate == 0:
+            plt.subplot(self.plots_count, 1, self.index)
+            plt.cla()
+            plt.xlabel(self.xlabel)
+            plt.ylabel(self.ylabel)
+            plt.plot(self.values)
+            if self.filter_size is not None:
+                plt.plot(filter_values(self.values, self.filter_size))
+            plt.pause(0.001)
