96dd5a7b56e9eb7570a1b2625e7452507542f940
Alexander Metz <alexander.wt.metz@gmail.com>
2020-01-23 23:44:13 +0100

    make consistent with readme

--- examples/simple.py
+++ examples/simple.py
@@ -2,9 +2,9 @@ from random import random
 
 from develocorder import LinePlot, set_recorder, record
 
-set_recorder(some_value=LinePlot())
+set_recorder(my_value=LinePlot())
 
 for _ in range(10):
-    record(some_value=random())
+    record(my_value=random())
 
 input("Press Enter to exit...")
