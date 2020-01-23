dab7b95142bf044ade2e7b29858425f23fec24ce
Alexander Metz <alexander.wt.metz@gmail.com>
2020-01-24 00:40:33 +0100

    increment version for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="develocorder",
-    version="0.1",
+    version="0.2.0",
     author="Alexander Wah Tak Metz",
     author_email="alexander.wt.metz@gmail.com",
     description="Simple live value plotter using Matplotlib",
