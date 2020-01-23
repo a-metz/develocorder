307fb04f3fa83ea5bd1059a872f03d87e386334b
Alexander Metz <alexander.wt.metz@gmail.com>
2020-01-23 21:47:08 +0100

    make example prettier

--- examples/example.py
+++ examples/example.py
@@ -4,32 +4,34 @@ from develocorder import LinePlot, Heatmap, set_recorder, record
 
 
 def setup():
-    set_recorder(loss=LinePlot(xlabel="Step", ylabel="Loss", filter_size=32, update_rate=10, max_size=500))
+    set_recorder(loss=LinePlot(xlabel="Step", ylabel="Loss", filter_size=64, update_rate=10))
+    set_recorder(loss_detail=LinePlot(xlabel="Step", ylabel="Loss Detail", update_rate=10, max_size=50))
+    set_recorder(action_values=Heatmap(xlabel="Step", ylabel="Action Values", update_rate=10, max_size=1000))
     set_recorder(score=LinePlot(xlabel="Episode", ylabel="Score"))
-    set_recorder(action_values=Heatmap(xlabel="Step", ylabel="Action Values", update_rate=10, max_size=200))
 
 
 def run():
-    for episode in range(10):
-        for step in range(100):
-            record(loss=example_loss(episode, step))
-            record(action_values=example_action_values(episode, step))
+    for episode in range(50):
+        for step in range(20):
+            i = episode * 20 + step
+            loss = example_loss(i)
+            record(loss=loss)
+            record(loss_detail=loss)
+            record(action_values=example_action_values(i))
 
         record(score=example_score(episode))
 
 
-def example_loss(episode, step):
-    i = episode * 100 + step + 1
-    return 10 / pow(i, 0.2) * (1 + 0.3 * np.random.standard_normal())
+def example_loss(i):
+    return 10 / (pow(i / 10, 0.5) + 1) * (1 + 0.3 * np.random.standard_normal())
 
 
-def example_score(episode):
-    return episode * (1 + 0.3 * np.random.standard_normal())
+def example_score(i):
+    return i * (1 + 0.1 * np.random.standard_normal())
 
 
-def example_action_values(episode, step):
-    i = episode * 100 + step + 1
-    return (0.01 * i) + pow(i, 0.2) * (1 + np.array([1, 0.3, 0, 0.1, 0.3]) * np.random.standard_normal(5))
+def example_action_values(i):
+    return np.array([1, 0.3, 0.2, 0.1, 0.3]) * i * 0.001 * (1 + 0.1 * np.random.standard_normal(5))
 
 
 if __name__ == "__main__":
