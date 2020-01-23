c1d02a9d9faf3e24715aa6509b91bd06b6d6d74e
Alexander Metz <alexander.wt.metz@gmail.com>
2020-01-23 13:02:53 +0100

    test graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,9 +3,33 @@ from unittest.mock import patch, MagicMock
 import develocorder.graph
 from develocorder.graph import *
 
+
+@patch("develocorder.graph._global_container_instance")
+def test_graph_base(container):
+    graph = GraphBase(xlabel="X", ylabel="Y", update_rate=2, max_size=3)
+    graph.draw_values = MagicMock()
+
+    container.add_axes.assert_called()
+    axes = container.add_axes.return_value
+
+    for i in range(10):
+        graph(i)
+
+    axes.clear.assert_called()
+    axes.set_xlabel.assert_called_with("X")
+    axes.set_ylabel.assert_called_with("Y")
+    container.refresh.assert_called()
+
+    # less calls due to update rate of 2
+    assert graph.draw_values.call_count == 5
+
+    # less values due to max size of 3
+    assert len(graph.values) == 3
+
+
 # patch magicmock into matplotlib backend
 @patch("develocorder.graph.matplotlib.pyplot")
-def test_graph_container(pyplot):
+def test_graph_container_pyplot_calls(pyplot):
     # test init
     container = GraphContainer()
 
