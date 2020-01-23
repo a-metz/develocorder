6a4609fbb2af048597100bd3b5ad068acee52814
Alexander Metz <alexander.wt.metz@gmail.com>
2020-01-23 23:37:34 +0100

    move filtering back to line plot

--- develocorder/line_plot.py
+++ develocorder/line_plot.py
@@ -1,4 +1,3 @@
-from .filter import filter_values
 from .graph import GraphBase
 
 
@@ -12,3 +11,11 @@ class LinePlot(GraphBase):
 
         if self.filter_size is not None:
             axes.plot(indices, filter_values(values, self.filter_size))
+
+
+def filter_values(values, filter_size):
+    padded_values = np.concatenate(
+        [np.full(filter_size // 2, values[0]), np.array(values), np.full(filter_size // 2 - 1, values[-1]),]
+    )
+    filter_kernel = np.ones((filter_size,)) / filter_size
+    return np.convolve(padded_values, filter_kernel, mode="valid")
