704be2d79d8c29b3df1981925be736b9e888e81f
Alexander Metz <alexander.wt.metz@gmail.com>
2020-02-09 15:13:12 +0100

    do not test interface between graph and container for more flexibility

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -4,55 +4,71 @@ import develocorder.graph
 from develocorder.graph import *
 
 
-@patch("develocorder.graph._global_container_instance")
-def test_graph_base(container):
-    graph = GraphBase(xlabel="X", ylabel="Y", update_rate=2, max_length=3)
-    graph.draw_values = MagicMock()
+# @patch("develocorder.graph._global_container_instance")
+# def test_graph_base(container):
+#     graph = GraphBase(xlabel="X", ylabel="Y", max_length=3)
+#     graph.draw_values = MagicMock()
 
-    container.add_axes.assert_called()
-    axes = container.add_axes.return_value
+#     container.register_graph.assert_called()
 
-    for i in range(10):
-        graph(i)
+#     for i in range(10):
+#         graph(i)
 
-    axes.clear.assert_called()
-    axes.set_xlabel.assert_called_with("X")
-    axes.set_ylabel.assert_called_with("Y")
-    container.refresh.assert_called()
+#     axes.clear.assert_called()
+#     axes.set_xlabel.assert_called_with("X")
+#     axes.set_ylabel.assert_called_with("Y")
+#     self.figure.canvas.draw
+#     container.refresh.assert_called()
 
-    # less calls due to update rate of 2
-    assert graph.draw_values.call_count == 5
+#     # less calls due to update rate of 2
+#     assert graph.draw_values.call_count == 5
 
-    # less values due to max size of 3
-    assert len(graph.values) == 3
+#     # less values due to max size of 3
+#     assert len(graph.values) == 3
 
 
 # patch magicmock into matplotlib backend
 @patch("develocorder.graph.matplotlib.pyplot")
-def test_graph_container_pyplot_calls(pyplot):
-    # test init
-    container = GraphContainer()
+@patch("develocorder.graph._global_container_instance", None)
+def test_pyplot_calls_on_container_creation(pyplot):
+    container = global_container_instance()
 
     pyplot.figure.assert_called()
     figure = pyplot.figure.return_value
     figure.show.assert_called()
 
-    # test adding axes
-    axes = MagicMock()
-    figure.add_subplot.return_value = axes
-    figure.axes = [axes]
 
-    container.add_axes()
+# patch magicmock into matplotlib backend
+@patch("develocorder.graph.matplotlib.pyplot")
+@patch("develocorder.graph._global_container_instance", None)
+def test_pyplot_calls_on_graph_creation(pyplot):
+    container = global_container_instance()
+    figure = container.figure
+
+    graph = GraphBase()
 
     figure.add_subplot.assert_called_with(1, 1, 1)
     axes = figure.add_subplot.return_value
 
-    container.add_axes()
+    graph = GraphBase()
 
     figure.add_subplot.assert_called_with(2, 1, 2)
+    # check that gemetry of first axis gets changed
     axes.change_geometry.assert_called_with(2, 1, 1)
 
-    # test refresh
-    container.refresh()
 
+# patch magicmock into matplotlib backend
+@patch("develocorder.graph.matplotlib.pyplot")
+@patch("develocorder.graph._global_container_instance", None)
+def test_pyplot_calls_on_graph_call(pyplot):
+    container = global_container_instance()
+    figure = container.figure
+    graph = GraphBase(xlabel="X", ylabel="Y")
+    axes = figure.add_subplot.return_value
+
+    graph(42)
+
+    axes.clear.assert_called()
+    axes.set_xlabel.assert_called_with("X")
+    axes.set_ylabel.assert_called_with("Y")
     figure.canvas.draw.assert_called()
