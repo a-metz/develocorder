9e7f8a8a7730ad15c628787a688b8b55c0ef0dea
Alexander Metz <alexander.wt.metz@gmail.com>
2020-02-09 16:46:38 +0100

    update tests

--- tests/test_heatmap_smoketest.py
+++ tests/test_heatmap_smoketest.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 import matplotlib
 
@@ -7,12 +9,13 @@ from develocorder import Heatmap, set_recorder, record
 @pytest.mark.filterwarnings(
     "ignore:Matplotlib is currently using agg, which is a non-GUI backend, so cannot show the figure"
 )
+@patch("develocorder.graph._global_container_instance", None)
 def test_heatmap_smoketest():
     # use backend which does not require a display for CI
     matplotlib.use("Agg")
 
     # all arguments
-    set_recorder(value1=Heatmap(xlabel="Sample", ylabel="Values 1", update_rate=2, max_length=3))
+    set_recorder(value1=Heatmap(xlabel="Sample", ylabel="Values 1", max_length=3))
 
     # no arguments
     set_recorder(value2=Heatmap())
--- tests/test_lineplot_smoketest.py
+++ tests/test_lineplot_smoketest.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 import matplotlib
 
@@ -7,16 +9,13 @@ from develocorder import LinePlot, set_recorder, record
 @pytest.mark.filterwarnings(
     "ignore:Matplotlib is currently using agg, which is a non-GUI backend, so cannot show the figure"
 )
+@patch("develocorder.graph._global_container_instance", None)
 def test_lineplot_smoketest():
     # use backend which does not require a display for CI
     matplotlib.use("Agg")
 
     # all arguments
-    set_recorder(
-        value1=LinePlot(
-            xlabel="Sample", ylabel="Value 1", filter_size=2, update_rate=2, max_length=3
-        )
-    )
+    set_recorder(value1=LinePlot(xlabel="Sample", ylabel="Value 1", filter_size=2, max_length=3))
 
     # no arguments
     set_recorder(value2=LinePlot())
