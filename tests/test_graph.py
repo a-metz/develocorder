02b3a44eeb1a6794008091e4b0cdc3e880861d56
Alexander Metz <alexander.wt.metz@gmail.com>
2020-02-09 16:27:11 +0100

    remove dead code

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -4,29 +4,6 @@ import develocorder.graph
 from develocorder.graph import *
 
 
-# @patch("develocorder.graph._global_container_instance")
-# def test_graph_base(container):
-#     graph = GraphBase(xlabel="X", ylabel="Y", max_length=3)
-#     graph.draw_values = MagicMock()
-
-#     container.register_graph.assert_called()
-
-#     for i in range(10):
-#         graph(i)
-
-#     axes.clear.assert_called()
-#     axes.set_xlabel.assert_called_with("X")
-#     axes.set_ylabel.assert_called_with("Y")
-#     self.figure.canvas.draw
-#     container.refresh.assert_called()
-
-#     # less calls due to update rate of 2
-#     assert graph.draw_values.call_count == 5
-
-#     # less values due to max size of 3
-#     assert len(graph.values) == 3
-
-
 # patch magicmock into matplotlib backend
 @patch("develocorder.graph.matplotlib.pyplot")
 @patch("develocorder.graph._global_container_instance", None)
