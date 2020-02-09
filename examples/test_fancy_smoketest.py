dfe9bcd26d260b9563fd6174402327ff6e94c545
Alexander Metz <alexander.wt.metz@gmail.com>
2020-02-09 16:47:17 +0100

    add examples as smoketests

---
+++ examples/test_fancy_smoketest.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+
+import pytest
+import matplotlib
+
+from fancy import run_example
+import develocorder
+
+
+@pytest.mark.filterwarnings(
+    "ignore:Matplotlib is currently using agg, which is a non-GUI backend, so cannot show the figure"
+)
+@patch("develocorder.graph._global_container_instance", None)
+def test_fancy_example_smoketest():
+    # use backend which does not require a display for CI
+    matplotlib.use("Agg")
+
+    run_example()
