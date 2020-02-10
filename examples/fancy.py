36a4b3239e36cc141368a2a8942f78c9f0a175b4
Alexander Metz <alexander.wt.metz@gmail.com>
2020-02-10 23:55:38 +0100

    improve code quality

--- examples/fancy.py
+++ examples/fancy.py
@@ -1,4 +1,5 @@
 import time
+from itertools import count
 
 import numpy as np
 
@@ -16,22 +17,22 @@ from develocorder import (
 
 def run_example():
     # axis labels
-    set_recorder(labeled=LinePlot(xlabel="Episode", ylabel="Score"))
+    set_recorder(labeled=LinePlot(xlabel="Step", ylabel="Score"))
 
     # additional filtered values (window filter)
-    set_recorder(filtered=FilteredLinePlot(filter_size=50, xlabel="Filtered"))
+    set_recorder(filtered=FilteredLinePlot(filter_size=50, xlabel="(Filtered)"))
 
     # maximum history length
-    set_recorder(detail=LinePlot(max_length=50, xlabel="Detail"))
+    set_recorder(detail=LinePlot(max_length=50, xlabel="(Detail)"))
 
     # downsampled values (record mean of every filter_size values)
-    set_recorder(downsampled=DownsampledLinePlot(filter_size=5, xlabel="Downsampled"))
+    set_recorder(downsampled=DownsampledLinePlot(filter_size=5, xlabel="(Downsampled)"))
 
     # multiple curves for recording 1d-array values
-    set_recorder(multiple=FilteredLinePlot(filter_size=50, xlabel="Multiple"))
+    set_recorder(multiple=FilteredLinePlot(filter_size=50, xlabel="(Multiple)"))
 
     # heatmap for recording 1d-array values
-    set_recorder(heatmap=Heatmap(xlabel="Heatmap"))
+    set_recorder(heatmap=Heatmap(xlabel="(Heatmap)"))
 
     # minimum update period (limit update rate for better performance)
     set_update_period(0.5)  # [seconds]
@@ -39,32 +40,37 @@ def run_example():
     # set number of columns
     set_num_columns(2)
 
-    # generate example values
-    for episode in range(50):
-        for step in range(20):
-            i = episode * 20 + step
-            loss = example_loss(i)
+    # example value generators
+    score_gen = score_values()
+    loss_gen = loss_values()
+    multiple = array_values(3)
+    heatmap = array_values(8)
+
+    for _ in range(50):
+        record(labeled=next(score_gen))
+        for _ in range(20):
+            loss = next(loss_gen)
             record(filtered=loss, downsampled=loss, detail=loss)
-            values = example_values(i)
-            record(multiple=values[:3], heatmap=values)
+            record(multiple=next(multiple), heatmap=next(heatmap))
             time.sleep(0.01)
 
-        record(labeled=example_score(episode))
+
+def loss_values(random=np.random.RandomState(0)):
+    for i in count():
+        yield 10 / (pow(i / 10, 0.5) + 1) * (1 + 0.3 * random.standard_normal())
 
 
-def example_loss(i):
-    return 10 / (pow(i / 10, 0.5) + 1) * (1 + 0.3 * np.random.standard_normal())
+def score_values(random=np.random.RandomState(0)):
+    for i in count():
+        yield i * (1 + 0.1 * random.standard_normal())
 
 
-def example_score(i):
-    return i * (1 + 0.1 * np.random.standard_normal())
-
-
-def example_values(i):
-    return np.array([1, 0.5, 0.2, 0.1, 0.3]) * i * 0.001 * (1 + 0.1 * np.random.standard_normal(5))
+def array_values(dimensions, random=np.random.RandomState(17)):
+    factor = random.random(dimensions) * 0.001
+    for i in count():
+        yield factor * i * (1 + 0.1 * random.standard_normal(dimensions))
 
 
 if __name__ == "__main__":
-    np.random.seed(0)
     run_example()
     input("Press Enter to exit...")
