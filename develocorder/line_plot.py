135c54417ad4c765d6c10cbe699f00820d8586a1
Alexander Metz <alexander.wt.metz@gmail.com>
2020-02-10 21:29:11 +0100

    add downsampled line plot

--- develocorder/line_plot.py
+++ develocorder/line_plot.py
@@ -37,3 +37,34 @@ def filter_values(values, filter_size):
     )
     filter_kernel = np.ones((filter_size,)) / filter_size
     return np.convolve(padded_values, filter_kernel, mode="valid")
+
+
+class DownsampledLinePlot(LinePlot):
+    """
+    Lineplot for recording scalar values. Recorded values are the mean of every filter_size
+    recorded values.
+    """
+
+    def __init__(self, filter_size, *args, **kwargs):
+        """
+        :param filter_size int: size of filter kernel (window)
+        """
+
+        super().__init__(*args, **kwargs)
+        self._filter_size = filter_size
+        self.filter_buffer = []
+
+    def __call__(self, value):
+        self.filter_buffer.append(value)
+
+        if len(self.filter_buffer) == self._filter_size:
+            filtered_value = np.mean(self.filter_buffer)
+            self.filter_buffer.clear()
+
+            super().__call__(filtered_value)
+
+    def draw_values(self, axes, indices, values):
+        downsampled_indices = range(
+            indices.start * self._filter_size, indices.stop * self._filter_size, self._filter_size
+        )
+        super().draw_values(axes, downsampled_indices, values)
--- examples/fancy.py
+++ examples/fancy.py
@@ -2,21 +2,32 @@ import time
 
 import numpy as np
 
-from develocorder import LinePlot, Heatmap, set_recorder, record, set_update_period, set_num_columns
+from develocorder import (
+    LinePlot,
+    Heatmap,
+    DownsampledLinePlot,
+    set_recorder,
+    record,
+    set_update_period,
+    set_num_columns,
+)
 
 
 def run_example():
-    # axis labels
-    set_recorder(labeled=LinePlot(xlabel="Episode", ylabel="Score"))
-
     # filter values (window filter kernel)
     set_recorder(filtered=LinePlot(filter_size=64))
 
     # maximum history length
     set_recorder(detail=LinePlot(max_length=50))
 
+    # downsampled values (record mean of every filter_size values)
+    set_recorder(downsampled=DownsampledLinePlot(filter_size=5))
+
     # show heatmap for recording 1d-array values
     set_recorder(array=Heatmap())
+
+    # axis labels
+    set_recorder(labeled=LinePlot(xlabel="Episode", ylabel="Score"))
 
     # minimum update period (limit update rate for better performance)
     set_update_period(0.5)  # [seconds]
@@ -29,7 +40,7 @@ def run_example():
         for step in range(20):
             i = episode * 20 + step
             loss = example_loss(i)
-            record(filtered=loss, detail=loss)
+            record(filtered=loss, downsampled=loss, detail=loss)
             record(array=example_values(i))
             time.sleep(0.01)
 
